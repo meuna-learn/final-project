b9f02d858eff73b7ee56d7096cec839e78de653b
anuragbg <anurag1998eng@gmail.com>
2025-03-22 22:28:26 +0530

    folder name change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import Header from './components/Header'
-import Footer from './components/Footer'
+import Hero from './components/Hero'
 import Main from './components/Main'
+import Testimonials from './components/Testimonials'
+import About from './components/About'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <>
+    <h1>Hello, I am under the water</h1>
       <Header/>
+      <Hero/>
       <Main/>
+      <Testimonials/>
+      <About/>
       <Footer/>
     </>
   );
